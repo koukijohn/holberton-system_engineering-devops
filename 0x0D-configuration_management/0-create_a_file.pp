bb9cdc87ed4e67faef3c5ad513bdf8f8288d26bd
koukijohn <koukijohn@github.com>
2018-07-26 22:07:56 +0000

    added 0-create_a_file.pp

---
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This is a puppet manifest taht creates a file in /tmp.
+
+file { '/tmp/holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love puppet'
+}
