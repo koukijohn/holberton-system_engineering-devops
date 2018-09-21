2abfeed0452058cb2430c8e31640cffc96fa9842
koukijohn <koukijohn@github.com>
2018-09-21 03:27:10 +0000

    added 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# This script will increase the limit from 15 to 15000
+exec { 'increase uLimit to 15000':
+  path    => '/bin',
+  command => "sed -i 's/15/15000/g' /etc/default/nginx",
+}
+
+exec { 'restart nginx':
+  path    => '/usr/bin',
+  command => "service nginx restart",
+}
